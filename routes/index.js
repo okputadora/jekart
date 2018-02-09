00272672c2c01cca0fdcedd0d155f40b88f0871c
okputadora <okputadora@gmail.com>
2018-02-09 12:14:53 -0500

    finished styling homepage

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express',
+    galleries: [
+      {title:'2017-2018'}, {title: 'Bedlam cups'}, {title: 'Wood Blocks'}]
+  });
 });
 
 module.exports = router;
