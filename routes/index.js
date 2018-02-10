1ab03cbea1714a19d3106417f1b48ada511f0a7f
okputadora <okputadora@gmail.com>
2018-02-10 18:22:11 -0500

    added the galleries page and styles

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,25 @@ router.get('/', function(req, res, next) {
   res.render('index', {
     title: 'Express',
     galleries: [
-      {title:'2017-2018'}, {title: 'Bedlam cups'}, {title: 'Wood Blocks'}]
+      {name:'2017-2018'}, {name: 'Bedlam cups'}, {name: 'Wood Blocks'}]
   });
 });
+
+router.get('/gallery', function(req, res, next){
+  res.render('gallery', {
+    title: 'Express',
+    galleries: [
+      {name:'2017-2018', imagePath: 'spaceman(minusframe).png'},
+      {name:'Bedlam cups', imagePath: 'focalpoints.png'},
+      {name:'Wood Blocks', imagePath: 'thirdfloor.png'},
+      {name:'2017-2018', imagePath: 'spaceman(minusframe).png'},
+      {name:'Bedlam cups', imagePath: 'focalpoints.png'},
+      {name:'Wood Blocks', imagePath: 'thirdfloor.png'},
+      {name:'2017-2018', imagePath: 'spaceman(minusframe).png'},
+      {name:'Bedlam cups', imagePath: 'focalpoints.png'},
+      {name:'Wood Blocks', imagePath: 'thirdfloor.png'}
+    ]
+  });
+})
 
 module.exports = router;
